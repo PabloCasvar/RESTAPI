6cccbf0f4d5aca81b3a1d26bdc538af9383d296c
PabloCasvar <casvar.pablo@gmail.com>
2017-04-09 06:04:16 -0500

    greeting service modified

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,8 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,9 @@ public class GreetingController{
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping("/greeting/{name}")
+    public Greeting greeting(@PathVariable(value="name") String name) {
+        if(name.equals("")) name = "World";
         return new Greeting(
                 counter.incrementAndGet(),
                 String.format(template, name)
